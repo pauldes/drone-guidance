12c472297f02a86fa000f0fdd1cec7cbadc451e6
pauldes <paul.desigaud@insa-lyon.fr>
2017-10-30 13:14:20 +0100

    double range filter

--- README.md
+++ README.md
@@ -9,5 +9,3 @@ npm install ar-drone
 ```
 
 2/ Lire la doc dans about_ar-drone.md
-
-3/ Le dossier node-ar-drone sert juste au cas où (je n'arrive pas à importer un module local)
--- vision/detect_circles_2.py
+++ vision/detect_circles_2.py
@@ -1,8 +1,10 @@
-##########################
-#
-#  FROM https://www.pyimagesearch.com/2014/07/21/detecting-circles-images-using-opencv-hough-circles/
-#Usage:
-#python detect_circles.py img.png
+'''
+P.Désigaud
+INSA Lyon
+github.com/pauldes
+A python algorithm using OpenCV's Hough Circles method to find a red circle in a picture
+Optimized for a 720p cheap camera (webcam, Parrot AR Drone)
+'''
 
 # import the necessary packages
 import numpy as np
@@ -14,48 +16,73 @@ if len(sys.argv) != 2:
     print("python detect_circles_2.py <image_circles_path>")
     exit()
 
-# load the image, clone it for output, and then convert it to grayscale
+# Load the image
 image = cv2.imread(sys.argv[1])
 output = image.copy()
-img = image.copy()
 
-#from https://pythonprogramming.net/color-filter-python-opencv-tutorial/
+#Convert to HSV color space
 hsv = cv2.cvtColor(image, cv2.COLOR_BGR2HSV)
-#TODO: still need adjustments
-lower_red = np.array([0,120,50])
-upper_red = np.array([40,220,150])
+
+# Red range 1 (hue from 0 to 40)
+lower_red1 = np.array([0,120,50])
+upper_red1 = np.array([40,220,150])
+# Red range 2 (hue from 140 to 180)
+lower_red2 = np.array([140,120,50])
+upper_red2 = np.array([180,220,150])
 # H: 0 - 180, S: 0 - 255, V: 0 - 255
-mask = cv2.inRange(hsv, lower_red, upper_red)
+
+#Creating the color masks
+mask1 = cv2.inRange(hsv, lower_red1, upper_red1)
+mask2 = cv2.inRange(hsv, lower_red2, upper_red2)
+mask = mask1|mask2
+
+#Apply the mask to the image
 img = cv2.bitwise_and(image,image, mask= mask)
-#######################################
+#Surprisingly it works when using only one mask:
+#img = cv2.bitwise_and(image,image, mask= mask1)
+
+#Convert to grayscale
 gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
 
-# detect circles in the image
+# Detect the circles
 circles = cv2.HoughCircles(gray, cv2.HOUGH_GRADIENT, 1, 20, param1=50,param2=30,minRadius=50,maxRadius=200)
 
-# ensure at least some circles were found
+# Ensure at least 1 circle was found
 if circles is not None:
-  # convert the (x, y) coordinates and radius of the circles to integers
+
+  # Convert the (x, y) coordinates and radius of the circles to integers
   circles = np.round(circles[0, :]).astype("int")
 
-  #Paul: find biggest r
-  biggest_r  = 0
-  biggest_circle = {0,0,0}
+  biggest_circle_r  = 0
+  biggest_circle_x = 0
+  biggest_circle_y = 0
 
+  #Iterating through the circles found
   for (x, y, r) in circles:
-    if(r>biggest_r):
-      biggest_circle = {x,y,r}
-      biggest_r = r
 
-  cv2.circle(output, (x, y), r, (0, 255, 0), 4)
-  cv2.rectangle(output, (x - 5, y - 5), (x + 5, y + 5), (0, 128, 255), -1)
+    #Look for the biggest circle
+    if(r>biggest_circle_r):
+      biggest_circle_r = r
+      biggest_circle_x = x
+      biggest_circle_y = y
+    # Draw the circle
+    cv2.circle(output, (x, y), r, (255, 0, 0), 2)
+    cv2.rectangle(output, (x - 5, y - 5), (x + 5, y + 5), (0, 128, 255), -1)
 
+  #Draw the biggest circle
+  cv2.circle(output, (biggest_circle_x, biggest_circle_y), biggest_circle_r, (0, 255, 0), 4)
+  cv2.rectangle(output, (biggest_circle_x - 5, biggest_circle_y - 5), (biggest_circle_x + 5, biggest_circle_y + 5), (0, 128, 255), -1)
+
+  #Draw input and output image
   cv2.namedWindow("output", cv2.WINDOW_NORMAL)
   cv2.imshow("output", np.hstack([image, output]))
 
 else:
   print('No circles were found!')
-  cv2.namedWindow("img", cv2.WINDOW_NORMAL)
-  cv2.imshow("img", img)
+
+cv2.namedWindow("img", cv2.WINDOW_NORMAL)
+cv2.imshow("img", img)
+cv2.namedWindow("gray", cv2.WINDOW_NORMAL)
+cv2.imshow("gray", gray)
 
 cv2.waitKey(0)
